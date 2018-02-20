45855473ea0d59707789607bd6a97f2369bbc1a0
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-02-20 23:19:37 +0000

    remaining path thing

--- tests/test_show_calls.py
+++ tests/test_show_calls.py
@@ -2,9 +2,9 @@ from __future__ import absolute_import, division, print_function
 
 import os
 
-def test_that_show_calls_does_not_crash():
+def test_that_show_calls_does_not_crash(testsdir):
+  t_dir = os.path.join(testsdir, 'valid')
   import fable.command_line.show_calls
-  t_dir = os.path.join(fable.__path__[0], 'test', 'valid')
   files = [f for f in os.listdir(t_dir) if f.endswith('.f') and f != 'blockdata_unnamed.f']
   assert files
   for filename in files:
