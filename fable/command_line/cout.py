106f1eb07ee043b8aa89495516cda37011db134f
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-02-28 22:49:53 +0000

    remove libtbx calls

--- fable/command_line/cout.py
+++ fable/command_line/cout.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, division
+import fable
 import fable.cout
 
 import hashlib
@@ -23,15 +24,14 @@ def check_fingerprint(file_name):
   return (orig_hexdigest == curr_hexdigest)
 
 def write_only_if_safe(file_name, text):
-  from libtbx.str_utils import show_string
   if os.path.exists(file_name):
     if not os.path.isfile(file_name):
       raise RuntimeError(
-        "Not a regular file: %s" % show_string(file_name))
+        "Not a regular file: %s" % file_name)
     stat = check_fingerprint(file_name=file_name)
     if (stat is None or not stat):
       raise RuntimeError(
-        "File appears to be manually modified: %s" % show_string(file_name))
+        "File '%s' appears to be manually modified" % file_name)
   hexdigest = compute_hexdigest(text=text)
   with open(file_name, "w") as f:
     f.write("// fingerprint %s\n" % hexdigest)
@@ -99,15 +99,11 @@ class process(object):
         easy_run.call(command=cmd)
 
 def run(args):
-  import libtbx.load_env
   if not args:
     args = ["--help"]
   elif args == ["--example"]:
-    args = [
-      libtbx.env.under_dist(module_name="fable", path="test/valid/sf.f"),
-      "--namespace", "example",
-      "--run"]
-  parser = optparse.OptionParser(usage="%s [options] fortran_file ..." % libtbx.env.dispatcher_name)
+    sys.exit("fable.cout tests/valid/sf.f --namespace example --run")
+  parser = optparse.OptionParser(usage="fable.cout [options] fortran_file ...")
   parser.add_option("-?", action="help", help=optparse.SUPPRESS_HELP)
   parser.add_option("--compile", action="store_true", default=False)
   parser.add_option("--link", action="store_true", default=False)
