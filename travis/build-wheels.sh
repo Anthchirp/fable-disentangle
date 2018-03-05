96463317c59f5c63ac9a28aa26c019f7058f9aac
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-03-05 21:58:16 +0000

    manylinux trials

---
+++ travis/build-wheels.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e -x
+
+# Install a system package required by our library
+yum install -y atlas-devel \
+    libboost-dev libboost-python-dev
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install pytest pytest-runner pytest-xdist # -r /io/dev-requirements.txt
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install -v fable --no-index -f /io/wheelhouse
+    (cd "$HOME"; "${PYBIN}/pytest" -v -n auto fable)
+done
